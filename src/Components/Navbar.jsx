5970463f2659e1deeaab16ab5272e93951c073b1
Naveen_S <naveen80567@gmail.com>
2024-01-01 00:29:41 +0530

    navbar bug fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
             <Link className="px-4 my-2 py-2 text-lg" onClick={handleClick} to='/research'>Research</Link>
             <Link className="px-4 my-2 py-2 text-lg" onClick={handleClick} to='/about'>About</Link>
     </nav> */}
-    <nav className={`${colour} w-2/3 h-screen flex flex-col absolute text-white border-0  border-r border-solid border-white transition-all duration-500 ease-in-out`+ (showMenu ? ' -left-0' : ' -left-full')}>
+    <nav className={`${colour} w-2/3 h-screen md:hidden flex flex-col absolute text-white border-0  border-r border-solid border-white transition-all duration-500 ease-in-out`+ (showMenu ? ' -left-0' : ' -left-full')}>
             <Link className="px-4 my-2 py-2 text-lg active:bg-zinc-200 active:bg-opacity-35 active:rounded-lg" onClick={handleClick} to='/home'>Home</Link>
             <Link className="px-4 my-2 py-2 text-lg active:bg-zinc-200 active:bg-opacity-35 active:rounded-lg" onClick={handleClick} to='/research'>Research</Link>
             <Link className="px-4 my-2 py-2 text-lg active:bg-zinc-200 active:bg-opacity-35 active:rounded-lg" onClick={handleClick} to='/about'>About</Link>
